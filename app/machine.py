75719e9f0210dd0ca5bf244217aef11a2e2a43c9
Rosalinda Burton <114356834+ROSEBURTON@users.noreply.github.com>
2023-07-19 10:17:44 -0700

    Create machine.py

---
+++ app/machine.py
@@ -0,0 +1,38 @@
+import datetime  # For getting the timestamp ex. Timestamp: 2023-01-16 2:56:47 PM
+from pandas import DataFrame  # getting predicted target to drop and fit other features
+from sklearn.ensemble import RandomForestClassifier  # choice of learning model
+from sklearn.tree import DecisionTreeClassifier  # 2nd choice comparison learning model
+import numpy as np
+import joblib  # Does save() properly ... to the specified filepath using joblib? 2nd using joblib??
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import GradientBoostingClassifier
+# 2 extra classification models in colab
+
+
+class Machine:  # machine.py for section named "view"
+    def __init__(self, df: DataFrame):
+        target = df["Rarity"]  # what we are aiming or feature targeted
+        features = df.drop(columns=["Rarity"])  # we drop the target to prevent model from cheating
+        self.model = RandomForestClassifier()  # DecisionTreeClassifier
+        self.name = {self.model}  # the name : RandomForestClassifier()
+        self.model.fit(features, target)  # fit the features in the data frame
+        self.timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")  #
+        # we want the date time .now in string format from the year down to second
+
+    def __call__(self, pred_basis: DataFrame) -> tuple:  # converting dataframe features to tuple
+        prediction = self.model.predict(pred_basis)  # this means: RandomForestClassifier.predict
+        probabilities = self.model.predict_proba(pred_basis)
+        confidence = np.max(probabilities, axis=1)
+        return prediction, confidence  # returning numerical rank number and confidence percentage
+
+    def info(self):  # the function returns info about model in the form of 2 strings
+        space = "<br>"  # using html to make a line break
+        return f"Base Model: Random Forest Classifier {space} Timestamp: {self.timestamp}"
+    # in the application we show the model chosen with a string and the proper time .now
+
+    def save(self, filepath):
+        pass  # model is displayed without this function
+
+    @staticmethod  # static method inside a class
+    def open(filepath):
+        pass  # model matches bandersnatch without this function
